99bca171ac9b2bd68db415d0918492e726a98e5e
ZvikaCohen <zvika96zvika@gmail.com>
2022-07-28 22:18:13 +0300

    Added main form with pictureboxes and listboxes.

--- FacebookWinFormsApp/FormMain.cs
+++ FacebookWinFormsApp/FormMain.cs
@@ -5,9 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-using FacebookWrapper.ObjectModel;
-using FacebookWrapper;
 
 namespace BasicFacebookFeatures
 {
@@ -16,29 +15,16 @@ namespace BasicFacebookFeatures
         public FormMain()
         {
             InitializeComponent();
-            FacebookWrapper.FacebookService.s_CollectionLimit = 100;
         }
 
-        private void buttonLogin_Click(object sender, EventArgs e)
+        private void FormMain_Load(object sender, EventArgs e)
         {
-            Clipboard.SetText("design.patterns20cc"); /// the current password for Desig Patter
 
-            FacebookWrapper.LoginResult loginResult = FacebookService.Login(
-                    /// (This is Desig Patter's App ID. replace it with your own)
-                    "1225204811548586", 
-                    /// requested permissions:
-					"email",
-                    "public_profile"
-                    /// add any relevant permissions
-                    );
-
-            buttonLogin.Text = $"Logged in as {loginResult.LoggedInUser.Name}";
         }
 
-        private void buttonLogout_Click(object sender, EventArgs e)
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-			FacebookService.LogoutWithUI();
-			buttonLogin.Text = "Login";
-		}
-	}
+
+        }
+    }
 }
