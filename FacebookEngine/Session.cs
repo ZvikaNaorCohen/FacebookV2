b7d8f43429ae99595a8c736573ec40639f5ba5ee
Roee Eisenberg <reisenr96@gmail.com>
2022-07-29 12:08:01 +0300

    Session class

---
+++ FacebookEngine/Session.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FacebookEngine
+{
+    public class Session
+    {
+        private FacebookWrapper.ObjectModel.User m_CurrentlyLoggedInUser;
+        private DateTime m_LastLoginTime;
+    }
+}
